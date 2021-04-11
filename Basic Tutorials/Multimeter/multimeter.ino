b476f260720d5e49cfb944358478bb0570f859ee
Jash Shah <47540320+Jash-2000@users.noreply.github.com>
2021-04-12 02:26:07 +0530

    Create multimeter.ino

---
+++ Basic Tutorials/Multimeter/multimeter.ino
@@ -0,0 +1,33 @@
+/*
+ * First program
+*/
+
+const int ledPin = 11;
+int level = 0;
+
+// run once at the beggining of the program
+void setup()
+{
+  // initializing pins as outputs
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+// super loop
+void loop()
+{
+ 
+  float voltage = 0.0;
+  analogWrite(ledPin, level);
+  delay(1000);
+  level = level + 10;
+  if( level >= 255 )
+  {
+  	level = 0;
+  }
+  Serial.print("LED output is : ");
+  Serial.println(level);
+  Serial.print("Voltage level is : ");
+  voltage =(5.0/255.0)*level;
+  Serial.println(voltage);
+}
